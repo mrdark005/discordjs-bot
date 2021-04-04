25927371380757cdfbe49d1c63bf87faa337730a
MrDark <mrd26897@gmail.com>
2021-04-04 13:35:03 +0600

    otorol

--- events/MetHods/otorol.js
+++ events/MetHods/otorol.js
@@ -1,3 +1,41 @@
-module.exports = memeber => {
-
-}
+module.exports = async member => {  
+    const Discord = require("discord.js"),
+              bot = new Discord.Client(),
+              { MessageEmbed } = require("discord.js"),
+              client = bot,
+              db = require("croxydb"),
+              ms = require('ms'),
+              time = ms("5s"),
+              guild = member.guild,
+              members = guild.memberCount,
+              {botClient} = require('./util/eventLoader.js')(bot)
+    
+      
+      if(db.has(`server_system.${member.guild.id}.auto_role`) === false) return;
+      let uye = member.guild.members.cache.size
+      let role = db.get(`server_system.${member.guild.id}.auto_role.member_role`);
+      let bot_role = db.get(`server_system.${member.guild.id}.auto_role.bot_role`);
+      let channel = db.get(`server_system.${member.guild.id}.auto_role.channel`);
+      if(!role) return;
+      if(!channel) return;
+      
+        if(!member.user.bot) {
+        member.roles.add(role)
+        const embed = new Discord.MessageEmbed()
+        .setColor("RANDOM")
+        .setTitle(`Mental | Otorol`)
+        .setDescription(`${member.user.tag} adlı kullanıcı sunucuya katıldı ve ona ayarlanmış olan rolü verdim.`)
+        .setTimestamp()
+        botClient.sendMessage((channel,embed))
+      } 
+        
+        else if(member.user.bot) {
+        member.roles.add(bot_role)
+        const embed = new Discord.MessageEmbed()
+        .setColor("RANDOM")
+        .setTitle(`Mental | Otorol`)
+        .setDescription(`<a:guvenli:775722659970940979> ${member.user.tag} adlı bot sunucuya katıldı ve ona ayarlanmış olan rolü verdim.`)
+        .setTimestamp()
+        botClient.sendMessage((channel,embed))
+      }
+    };
--- komutlar/otorol-sistemi.js
+++ komutlar/otorol-sistemi.js
@@ -1,9 +1,35 @@
 const Discord = require("discord.js");
 var db = require("croxydb")
-const moment = require("moment")
 exports.run = async (client, message, args) => {
-//
-
+    if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply("Yetersiz yetki");
+    if (!args[0]) return client.errorMessage(msg.channel.id,"ayarla rol [bot,üye] <rol>, ayarla kanal <kanal>, sıfırla");  
+    if (args[0] === "ayarla") {
+        if (db.has(`server_system.${message.guild.id}.auto_role`) === true) return client.errorMessage(msg.channel.id,"Sistem zaten aktif")
+        else if (args[1] === "rol") {
+            var parametreler = ["bot", "üye"];
+            if (!parametreler.includes(args[2])) return client.errorMessage(msg.channel.id,"Bot veya Üye yazmalısın.");
+            if (args[2] === "bot") {
+                var bot_role = message.mentions.roles.first();
+                db.set(`server_system.${message.guild.id}.auto_role.bot_role`,bot_role.id);
+                message.channel.send("Bot rolü ayarlandı."); 
+            } 
+            else if (args[2] === "üye") {
+                var member_role = message.mentions.roles.first();
+                db.set(`server_system.${message.guild.id}.auto_role.member_role`,member_role.id);
+                message.channel.send("Üye rolü ayarlandı.");
+            }
+        } 
+        else if (args[1] === "kanal") {
+            var channel = message.mentions.channels.first();
+            db.set(`server_system.${message.guild.id}.auto_role.channel`,channel.id);
+            message.channel.send("Kanal ayarlandı.");
+        }
+    }
+    if (args[0] === "sıfırla") {
+        if (db.has(`server_system.${message.guild.id}.auto_role`) === false) return client.errorMessage(msg.channel.id,"Sistem zaten kapalı");
+        db.delete(`server_system.${message.guild.id}.auto_role`);
+        message.reply("Otorol sistemi kapatıldı.");
+  }
 }
 exports.conf = {
     enabled: true,
